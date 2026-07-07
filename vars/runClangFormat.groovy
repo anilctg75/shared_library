fbf5e92db23e0fca0c7228b572445a552d8c02a2
sandeepds077 <115927838+sandeepds077@users.noreply.github.com>
2026-07-08 00:26:09 +0530

    Fix escaping for variables in clang-format checks

--- vars/runClangFormat.groovy
+++ vars/runClangFormat.groovy
@@ -12,9 +12,9 @@ def call(String projectDir = '.', String buildDir = 'build') {
 
   if (sh(script: 'command -v clang-format >/dev/null 2>&1 || echo no', returnStdout: true).trim() != 'no') {
     sh "echo 'Checking C files...' >> ${report}"
-    sh "find ${projectDir}/src ${projectDir}/include -type f -name \"*.c\" -print0 | xargs -0 --no-run-if-empty sh -c 'clang-format --dry-run --Werror \"\\$@\" 2>&1' sh >> ${report} || true"
+    sh "find ${projectDir}/src ${projectDir}/include -type f -name \"*.c\" -print0 | xargs -0 --no-run-if-empty sh -c 'clang-format --dry-run --Werror \"\$@\" 2>&1' sh >> ${report} || true"
     sh "echo 'Checking H files...' >> ${report}"
-    sh "find ${projectDir}/src ${projectDir}/include -type f -name \"*.h\" -print0 | xargs -0 --no-run-if-empty sh -c 'clang-format --dry-run --Werror \"\\$@\" 2>&1' sh >> ${report} || true"
+    sh "find ${projectDir}/src ${projectDir}/include -type f -name \"*.h\" -print0 | xargs -0 --no-run-if-empty sh -c 'clang-format --dry-run --Werror \"\$@\" 2>&1' sh >> ${report} || true"
   } else {
     sh "echo 'clang-format not found; skipping lint' >> ${report}"
   }
